613c4c2a1d49a6d55159b75a7025e1100e32b692
dimkarasev <152750301+dimkarasev@users.noreply.github.com>
2024-08-26 19:20:21 +1000

    no more shitty replace

--- scripts/beautyfulprint.py
+++ scripts/beautyfulprint.py
@@ -2,18 +2,18 @@ import time
 
 def exe(word):
     for char in word:
-        print(char.replace("⡀", " "), end='', flush=True)
+        print(char, end='', flush=True)
         time.sleep(0.005)
     print ()
     
 def exeloong(word):
     for char in word:
-        print(char.replace("⡀", " "), end='', flush=True)
+        print(char, end='', flush=True)
         time.sleep(0.03)
     print ()
     
 def wait():
     for char in '...':
-        print(char.replace("⡀", " "), end='', flush=True)
+        print(char, end='', flush=True)
         time.sleep(1.5)
     print ()
